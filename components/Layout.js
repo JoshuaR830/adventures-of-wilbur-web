7102c73f7f4a706e37d17d36ea539dafe0a84d94
Joshua <joshuaquarryhouse@gmail.com>
2021-10-23 13:25:11 +0100

    Added a new Wilbur inspired theme and changed nav so that it is suitable for different device sizes

--- components/Layout.js
+++ components/Layout.js
@@ -3,11 +3,11 @@ import Nav from './Navigation'
 
 export default function Layout({children}) {
     return (
-        <>
+        <div className={layoutStyles.container}>
             <Nav></Nav>
             <div className={layoutStyles.layout}>
                 <main>{children}</main>
             </div>
-        </>
+        </div>
     )
 }
--- components/Navigation.js
+++ components/Navigation.js
@@ -4,11 +4,13 @@ import NavItem from './NavigationItem'
 
 export default function Nav() {
     return (
-        <div className={navStyles.nav}>
-            <NavItem text="Feed" iconName="/images/feed.svg" link={"/"}></NavItem>
-            <NavItem text="Latest" iconName="/images/latest.svg" link={"/latest"}></NavItem>
-            <NavItem text="Story" iconName="/images/story.svg" link={"/story"}></NavItem>
-            <NavItem text="Random" iconName="/images/random.svg" link={"/random"}></NavItem>
+        <div className={navStyles.wrapper}>
+            <div className={navStyles.nav}>
+                <NavItem text="Feed" iconName="/images/feed.svg" link={"/"}></NavItem>
+                <NavItem text="Latest" iconName="/images/latest.svg" link={"/latest"}></NavItem>
+                <NavItem text="Story" iconName="/images/story.svg" link={"/story"}></NavItem>
+                <NavItem text="Random" iconName="/images/random.svg" link={"/random"}></NavItem>
+            </div>
         </div>
     )
 }
