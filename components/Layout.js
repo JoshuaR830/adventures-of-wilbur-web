b6901577effb7a5e232a655b42dd4fde5200efe4
Joshua <joshuaquarryhouse@gmail.com>
2021-11-06 12:51:18 +0000

    Added load more button

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,17 @@
 import layoutStyles from './Layout.module.scss'
 import Nav from './Navigation'
+import Head from 'next/head'
+import Script from 'next/script'
 
 export default function Layout({children}) {
     return (
+        <>
         <div className={layoutStyles.container}>
             <Nav></Nav>
             <div className={layoutStyles.layout}>
                 <main>{children}</main>
             </div>
         </div>
+        </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -4,32 +4,100 @@ import styles from '../styles/Feed.module.scss'
 import Card from '../components/Card'
 import Highlight from '../components/Highlight'
 import Layout from '../components/Layout'
+import useSWRInfinite from 'swr/infinite'
+
+let maxIndex = 0;
 
 export async function getServerSideProps() {
-  const data = await fetch("http://18.134.140.176:2048/WilburFeed");
-  const postData = await data.json();
-
+  // const data = await fetch("http://18.134.140.176:2048/WilburFeed");
+  process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+  const res = await fetch(`https://localhost:5001/GetMostRecentIndex`, {'mode': 'cors'});
+  
+  const res2 = await fetch(`https://localhost:5001/WilburPagedItemNewestFirst?page=1&limit=1`, {'mode': 'cors'});
+  console.log(res)
+  const index = await res.json();
+  const pageData = await res2.json();
+  console.log(index)
+  maxIndex = index;
   return {
     props: {
-      postData
+      pageData
     }
   }
 }
 
-export default function Home({postData}) {
+const getKey = (pageIndex, previousPageData) => {
+  console.log("Prev")
+  console.log(previousPageData)
+
+  if (pageIndex === 0 && previousPageData === null) {
+    return `https://localhost:5001/WilburPagedItemNewestFirst?page=${pageIndex}&limit=${6}`
+  }
+
+  console.log(previousPageData.length)
+
+  if(previousPageData && !previousPageData.length) {
+    console.log("Woop")
+    return null;
+  }
+
+  
+  console.log(pageIndex)
+
+  return `https://localhost:5001/WilburPagedItemNewestFirst?page=${pageIndex}&limit=${6}`
+}
+
+const fetcher = async (url) => {
+  console.log(url);
+  const res = await fetch(url, {'mode': 'cors'});
+  const posts = await res.json();
+  console.log(res.body)
+  console.log(`post ${posts}`)
+
+  console.log("hello")
+  console.log(posts)
+
+  return posts
+}
+
+export default function Home({pageData}) {
+
+  const {data, size, setSize} = useSWRInfinite(getKey, fetcher)
+
+  if (!data) return "loading"
+
+  console.log(data)
+  
+  console.log(`size ${size}`)
+
+  var postCount = 0;
+  for(var i = 0; i < data.length; i ++) {
+    postCount += data[i].length
+  }
+
   return (
     <Layout>
-      <Highlight id={19} title={"Wilbur's Wilderness Weekend - Campfire"} body={"Uh oh, that looks like fire, Wilbur, Wilbur! Are you ok? Fire is dangerous Wilbur! You will singe your fur. Come away from the fire Wilbur. How did you even manage to light the campfire? Phew, Wilbur says he's fine, he just wanted to get the whole wilderness experience by going wild camping, and in true camping style, the rain is pouring. Wilbur does not like getting wet, so he has set up a nice tent for himself. Consequently he is keeping his distance from the fire - at the moment at least. Thank goodness for that!"} wilburImage={"https://adventures-of-wilbur-images.s3.eu-west-2.amazonaws.com/Wilbur_2020_8_15_17_56_58_109.jpg"}></Highlight>
-      
+
+      {/* <script dangerouslySetInnerHTML={{ __html:
+          `let observer = new IntersectionObserver(function(entries) {
+              if(entries[0].isIntersecting === true)
+                  console.log("Item visible")
+              }, {threshold: [1]})
+              
+          observer.observe(document.querySelector(Highlight))
+              `
+          
+              }}>
+      </script> */}
+
+      {/* <Highlight id={19} title={"Wilbur's Wilderness Weekend - Campfire"} body={"Uh oh, that looks like fire, Wilbur, Wilbur! Are you ok? Fire is dangerous Wilbur! You will singe your fur. Come away from the fire Wilbur. How did you even manage to light the campfire? Phew, Wilbur says he's fine, he just wanted to get the whole wilderness experience by going wild camping, and in true camping style, the rain is pouring. Wilbur does not like getting wet, so he has set up a nice tent for himself. Consequently he is keeping his distance from the fire - at the moment at least. Thank goodness for that!"} wilburImage={"https://adventures-of-wilbur-images.s3.eu-west-2.amazonaws.com/Wilbur_2020_8_15_17_56_58_109.jpg"}></Highlight> */}
+    
       <div className={styles.flexContainer}>
-        {postData.map(({id, title, body, wilburImage}) => (
-          <Card id={id} title={title} body={body} wilburImage={`https://adventures-of-wilbur-images.s3.eu-west-2.amazonaws.com/${wilburImage}`}/>
-        ))}
-        {/* <Card id={278} title={"Delays in Duck Design Down to Deliberation"} body={"Wilbur and Mr Snuggles took their time to put the pieces together. After much deliberation about what the bricks were for they decided that they could design and build a duck! "} wilburImage={"https://adventures-of-wilbur-images.s3.eu-west-2.amazonaws.com/Wilbur_2021_4_14_13_15_55_480.jpg"}></Card>
-        
-        <Card id={7} title={"Wilbur Pauses in the Pond with his Paws in the Pond while Precipitation Pours in the Pond"} body={"Wilbur has been a busy Marmot today! It looks like he is taking a nice long bath in the pond, one of his favourite things to do. Loving the shower cap and wellies Wilbur! Don't forget to clean those teeth of yours! And make sure that you dry off before coming back into the house!"} wilburImage={"https://adventures-of-wilbur-images.s3.eu-west-2.amazonaws.com/Wilbur_2020_8_15_16_37_23_29.jpg"}></Card>
-        
-        <Card id={296} title={"Waking Up To Wilbur's Wonderful Waffles"} body={"Wilbur has developed a rather wonderful new skill - waffle making! Nothing like waking up to fresh, waffles by Wilbur!"} wilburImage={"https://adventures-of-wilbur-images.s3.eu-west-2.amazonaws.com/Wilbur_2021_8_18_7_21_25_364.jpg"}></Card> */}
+        {data.map((posts, index) => {
+          return posts.map(({id, title, body, wilburImage}) => (
+            <Card key={id} id={id} title={title} body={body} wilburImage={`https://adventures-of-wilbur-images.s3.eu-west-2.amazonaws.com/${wilburImage}`}/>
+        ))})}
+        <button onClick={() => setSize(size + 1)} >More</button>
       </div>
     </Layout>
   )
